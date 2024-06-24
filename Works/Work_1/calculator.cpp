e32957295b5c46ed6468808318ab0112cb79ae4f
artashyeritsyan <yeritsyanartash@gmail.com>
2024-06-25 00:38:28 +0400

    Wrote parser, and made some changes

--- Works/Work_1/calculator.cpp
+++ Works/Work_1/calculator.cpp
@@ -1,21 +1,55 @@
 #include "calculator.hpp"
 
-void Calculator::parser(std::string expr, Node* ptr){
-    int op_index = 0;
-    for(int i = 0; i < expr.length(); ++i){
-        if(is_sum_or_sub(expr[i])){
-            ptr->op = expr[i];
-            op_index = i;
-            break;
-        }
-        if (is_div_or_mul(expr[i]) && !ptr->op) {
-            ptr->op = expr[i];
-            op_index = i;
-        }
+void Calculator::parser(std::string expr, Node* ptr) {
+    if(expr.empty()) {
+        return;
     }
+
+    if (expr[0] == '(' && expr[expr.length() - 1] == ')' ) {
+        expr = expr.substr(1, expr.length() - 2);
+    }
+
+    int op_index = find_op_index(expr, ptr);
+    // [1*2+6-3/7]
+    ptr = new Node;
+    ptr->op = expr[op_index];
+
+    auto left = expr.substr(0, op_index);
+    auto right = expr.substr(op_index + 1, expr.length() - op_index - 1);
+
+    parser(left, ptr->left);
+    parser(right, ptr->right);
 }
 
-double Calculator::calculate(std::string expr){
+int Calculator::find_op_index(std::string expr, Node *ptr) {
+    int op_index = 0;
+    int brackets_level = 0;
+
+    for(int i = 0; i < expr.length(); ++i){
+        if (expr[i] == '(') {
+            brackets_level++;
+        }
+        else if (expr[i] == ')') {
+            brackets_level--;
+        }
+
+        if(brackets_level == 0) {
+            if (is_sum_or_sub(expr[i])){
+                ptr->op = expr[i];
+                op_index = i;
+                break;
+            }
+            else if (is_div_or_mul(expr[i]) && !ptr->op) {
+                ptr->op = expr[i];
+                op_index = i;
+            }
+        }
+    }
+
+    return op_index;
+}
+
+double Calculator::calculate(std::string expr) {
     expr = erase_remove_spaces(expr);
     parser(expr, head);
     return 0.0;
@@ -23,12 +57,12 @@ double Calculator::calculate(std::string expr){
 
 
 
-std::string Calculator::erase_remove_spaces(std::string expr){
+std::string Calculator::erase_remove_spaces(std::string expr) {
     expr.erase(std::remove(expr.begin(), expr.end(), ' '), expr.end());
     return expr;
 }
 
-bool Calculator::is_sum_or_sub(const char &c){
+bool Calculator::is_sum_or_sub(const char &c) {
     bool result = false;
     if(c == '+' || c == '-'){
         result = true;
@@ -36,7 +70,7 @@ bool Calculator::is_sum_or_sub(const char &c){
     return result;
 }
 
-bool Calculator::is_div_or_mul(const char &c){
+bool Calculator::is_div_or_mul(const char &c) {
     bool result = false;
     if(c == '*' || c == '/'){
         result = true;

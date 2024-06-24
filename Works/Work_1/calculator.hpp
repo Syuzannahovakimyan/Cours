64ecd8fa0a82d2c49279da265ae0c07d2a868d0d
artashyeritsyan <yeritsyanartash@gmail.com>
2024-06-25 00:38:28 +0400

    Wrote parser, and made some changes

--- Works/Work_1/calculator.hpp
+++ Works/Work_1/calculator.hpp
@@ -12,15 +12,18 @@ class Calculator{
                 Node* left  = nullptr;
                 Node* right = nullptr;
         };
-        Node* head;
-        void parser(std::string expr, Node* ptr);
+
+    Node* head;
+    void parser(std::string expr, Node* ptr);
+    std::string erase_remove_spaces(std::string expr);
+    bool is_sum_or_sub(const char& c);
+    bool is_div_or_mul(const char& c);
+    int find_op_index(std::string expr, Node* ptr);
+
     public:
         Calculator(){}
         ~Calculator(){}
 
         double calculate(std::string expr);
-        std::string erase_remove_spaces(std::string expr);
-        bool is_sum_or_sub(const char& c);
-        bool is_div_or_mul(const char& c);
 
 };
